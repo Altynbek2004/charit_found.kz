0523729cc9afda848b86f989b50d4e0a09a6cd40
Qalaudev <maksatdaniarbekly@gmail.com>
2025-04-09 17:19:38 +0500

    added design register form, create animal form

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import ProductList from '../components/ProductList.vue';
 import EditProduct from '../components/EditProduct.vue';
 import CreateProduct from '../components/CreateProduct.vue';
+import CreateAnimal from "../components/CreateAnimal.vue";
+import Registration from "../components/Registration.vue";
 
 const routes = [
     {
@@ -12,10 +14,19 @@ const routes = [
         path: '/products/create',
         component: CreateProduct,
     },
+
     {
         path: '/products/:id/edit',
         component: EditProduct,
     },
+    {
+        path: '/create-animal',
+        component: CreateAnimal,
+    },
+    {
+        path: '/register',
+        component: Registration,
+    }
 ];
 
 const router = createRouter({
